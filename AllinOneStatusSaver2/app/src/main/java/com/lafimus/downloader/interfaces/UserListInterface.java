4a67abea29e9a28c9c6f5a67c4c956a9a25b2c8c
Ismayil Muxtarov <muxtarovismayil312@gmail.com>
2020-09-30 17:24:11 +0400

    lafimus

---
+++ AllinOneStatusSaver2/app/src/main/java/com/lafimus/downloader/interfaces/UserListInterface.java
@@ -0,0 +1,8 @@
+package com.lafimus.downloader.interfaces;
+
+
+import com.lafimus.downloader.model.story.TrayModel;
+
+public interface UserListInterface {
+    void userListClick(int position, TrayModel trayModel);
+}
